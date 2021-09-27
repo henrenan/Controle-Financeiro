cb3ebaa93bbace8b3a81a6ccc09162b5e9f82e4b
Renan Marques <henrenan@gmail.com>
2021-09-26 22:35:25 -0300

    dados

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
 
 const dummyTransactions = [
     {id: 1, name: 'Bolo de brigadeiro', amount: -20},
@@ -21,9 +24,23 @@ const addTransactionIntoDOM = transaction => {
 }
  
 const updateBalanceValues = () => {
-    const transactionsAmount = dummyTransactions.map(transaction => transaction.amount)
-    const total = transactionsAmount.reduce((accumulator,transaction) => accumulator + transaction, 0)
-    console.log(total)
+    const transactionsAmount = dummyTransactions
+        .map(transaction => transaction.amount)
+    const total = transactionsAmount
+        .reduce((accumulator,transaction) => accumulator + transaction, 0)
+        .toFixed(2)
+    const income = transactionsAmount
+        .filter(value => value > 0)
+        .reduce((accumulator,value) => accumulator + value, 0)
+        .toFixed(2)
+    const expense = Math.abs(transactionsAmount.filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value, 0))
+        .toFixed(2)
+    
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
+    
 }
 
 const init = () => {
